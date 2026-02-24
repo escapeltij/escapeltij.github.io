ce86f2161ec36f026705d508cccd69a13e6727a1
escapeltij <kirschuletz@gmail.com>
2026-02-24 16:58:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const daysElement = document.getElementById("days");
     
     if (daysElement) {
-        const releaseDate = new Date("2026-02-25T20:00:00+02:00").getTime();
+        const releaseDate = new Date("2026-03-09T20:00:00+02:00").getTime();
 
         const timer = setInterval(function() {
             const now = new Date().getTime();
